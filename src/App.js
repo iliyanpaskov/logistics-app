6b94275e018fd12ec1b188fb19025cf4c083a607
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-02-24 18:41:28 +0200

    Contacts phone section added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
+import Services from "./components/Services/Services";
+import Contacts from "./components/Contacts/Contacts";
 
 import './App.css'
-import Services from "./components/Services/Services";
 
 function App() {
   return (
@@ -15,11 +16,10 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About/>}/>
-          <Route path="/services" element={<Services/>}/>
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contacts" element={<Contacts />} />
         </Routes>
-
-
       </main>
       <Footer />
     </div>
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,11 +1,16 @@
-import style from "./Contacts.module.css"
+import style from "./Contacts.module.css";
 
 const Contacts = () => {
     return (
         <article className={style["contacts-section"]}>
-            <section className={style["contacts-map"]}>
-
+            <section className={style["contacts-phone"]}>
+                <article className={style["contacts-phone-wrapper"]}>
+                    <i className="fa-solid fa-phone"></i>
+                    <h1 className={style["contacts-phone-number"]}>24/7 Call Center at +012 333 4444 </h1>
+                </article>
+                <img src="/images/team-8.jpg" alt="Call center" />
             </section>
+
             <section className={style["contacts-form-wrapper"]}>
                 <h4 className={style["contacts-form-pre-title"]}>contact us</h4>
                 <h1 className={style["contacts-form-title"]}>Contact For Any Queries</h1>
