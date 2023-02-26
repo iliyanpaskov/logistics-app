1a57953ebdb42760ab279a2df3bf24961e091d8d
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-02-26 13:03:13 +0200

    ServiceCard component btn added

--- src/components/Services/ServicesCard/ServicesCard.js
+++ src/components/Services/ServicesCard/ServicesCard.js
@@ -1,10 +1,12 @@
 import style from "./ServicesCard.module.css"
+import { Link } from "react-router-dom";
 
 const ServicesCard = ({
     service
 }) => {
     return (
-            <li className={style["list-item"]}>
+        <li className={style["list-item"]}>
+            <section className={style["list-item-content-wrapper"]}>
                 <section className={style["list-item-img-wrapper"]} >
                     <img src={service.serviceImage.url} alt="" />
                 </section>
@@ -12,7 +14,9 @@ const ServicesCard = ({
                     <h2 className={style["list-item-content-title"]}>{service.service}</h2>
                     <p className={style["list-item-content-text"]}>{service.description}</p>
                 </section>
-            </li>
+            </section>
+            <Link to={service.objectId} className={style["list-item-btn"]}> read more</Link>
+        </li>
     );
 }
 
